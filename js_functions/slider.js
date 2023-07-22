0d86585ada3b57d2537564ab1bf914d3c7088957
Stella <122531435+intrastella@users.noreply.github.com>
2023-07-22 15:13:06 +0200

    Update slider.js

--- js_functions/slider.js
+++ js_functions/slider.js
@@ -8,14 +8,20 @@ function set_slider() {
     let screen_width = $(window).width();
     const center = screen_width * 0.5;
 
+    alert("window");
+
     const width_slide = 850;
     const start = center - (1.5 * width_slide);
     const post2 = start + width_slide;
 
+    alert("post params");
+
 
         // Select all slides
     const slides = document.querySelectorAll(".slide");
+    alert("slides");
     const post_cards = document.querySelectorAll(".post_card");
+    alert("post card");
     const post_title = document.querySelectorAll(".post_title");
 
     alert("start with setup carousel");
