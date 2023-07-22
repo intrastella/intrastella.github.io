07e0b0bb0ec2964983614718aab526a88f9ccd60
stella <stellamuambangufulu@gmail.com>
2023-07-23 01:40:53 +0200

    update tpoics

--- js_functions/resize_subject.js
+++ js_functions/resize_subject.js
@@ -25,18 +25,19 @@ function set_pos() {
         subject_page.style.height = "2400px";
 
 
-        var intro_pos = window.innerWidth * 0.5 - 350;
+        var intro_pos = window.innerWidth * 0.5 - 380;
         intro_space.style.left = `${intro_pos}px`;
+        intro_space.style.top = "110px";
 
         var sub_top_pos = window.innerWidth * 0.5 - 500;
-        top_grid.style.top = "650px";
+        top_grid.style.top = "700px";
         top_grid.style.left = `${sub_top_pos}px`;
 
         const new_grid = "grid-template-columns: 470px 470px; grid-template-rows: 470px 470px; column-gap: 87px; row-gap: 80px;";
         bottom_grid.style.cssText = new_grid;
 
         var sub_bottom_pos = window.innerWidth * 0.5 - 500;
-        bottom_grid.style.top = "1220px";
+        bottom_grid.style.top = "1270px";
         bottom_grid.style.left = `${sub_bottom_pos}px`;
 
         subjects.forEach((subject, idx) => {
@@ -86,19 +87,19 @@ function set_pos() {
         top_grid.style.top = "110px";
         top_grid.style.left = "45%";
 
-        const old_grid = "display: grid;grid-template-columns: 320px 320px 320px 320px;grid-template-rows: auto auto auto auto;column-gap: 25px;top: 650px;left: 29%;position: absolute;padding: 0px;width: 90%;"
+        const old_grid = "display: grid;grid-template-columns: 260px 260px 260px 260px;grid-template-rows: auto auto auto auto;column-gap: 3px;top: 680px;left: 45.5%;position: absolute;padding: 0px;width: 90%;"
         bottom_grid.style.cssText = old_grid;
 
         subjects.forEach((subject, idx) => {
 
-            subject.style.width = "280px";
-            subject.style.height = "280px";
+            subject.style.width = "200px";
+            subject.style.height = "200px";
 
             const border_secs = subject.querySelectorAll('.border_sec');
 
             border_secs.forEach((border_sec, idx) => {
-                border_sec.style.width = "220px";
-                border_sec.style.height = "220px";
+                border_sec.style.width = "160px";
+                border_sec.style.height = "160px";
             });
 
             const txt_subject0s = subject.querySelectorAll('.txt_subject0');
@@ -106,25 +107,25 @@ function set_pos() {
             const txt_subject2s = subject.querySelectorAll('.txt_subject2');
 
             txt_subject0s.forEach((txt_subject0, idx) => {
-                txt_subject0.style.fontSize = "20px";
+                txt_subject0.style.fontSize = "15px";
             });
 
             txt_subject1s.forEach((txt_subject1, idx) => {
-                txt_subject1.style.fontSize = "20px";
-                txt_subject1.style.left = "25px";
+                txt_subject1.style.fontSize = "15px";
+                txt_subject1.style.left = "40px";
             });
 
             txt_subject2s.forEach((txt_subject2, idx) => {
-                txt_subject2.style.fontSize = "20px";
-                txt_subject2.style.right = "25px";
+                txt_subject2.style.fontSize = "15px";
+                txt_subject2.style.right = "40px";
             });
 
         });
 
-        document.getElementById("diffgeo").style.backgroundSize = "280px";
-        document.getElementById("funk").style.backgroundSize = "280px";
-        document.getElementById("stat").style.backgroundSize = "280px";
-        document.getElementById("topo").style.backgroundSize = "280px";
+        document.getElementById("diffgeo").style.backgroundSize = "200px";
+        document.getElementById("funk").style.backgroundSize = "200px";
+        document.getElementById("stat").style.backgroundSize = "200px";
+        document.getElementById("topo").style.backgroundSize = "200px";
 
     }
 
