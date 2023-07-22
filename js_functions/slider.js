643aba7e01443efcd7c664f427f17a0bfcb69f50
Stella <122531435+intrastella@users.noreply.github.com>
2023-07-22 16:08:50 +0200

    Update slider.js

--- js_functions/slider.js
+++ js_functions/slider.js
@@ -3,10 +3,11 @@ function set_slider() {
 
     alert("Function is being called");
 
-    var screen_width = window.innerWidth;
+    // var screen_width = window.innerWidth;
     // var h = window.innerHeight;
     
     // let screen_width = $(window).width();
+    var screen_width = 1900;
     const center = screen_width * 0.5;
 
     const width_slide = 850;
